9ca56f264c8cfdca8b60d4d344c91ec787c21ef0
Alan Camargo <alan.uk.br@gmail.com>
2023-04-26 19:25:14 -0300

    Implement repository

--- feature-filters/src/main/java/com/alancamargo/hearthstone/filters/di/FiltersModule.kt
+++ feature-filters/src/main/java/com/alancamargo/hearthstone/filters/di/FiltersModule.kt
@@ -4,6 +4,8 @@ import com.alancamargo.hearthstone.filters.data.local.FiltersLocalDataSource
 import com.alancamargo.hearthstone.filters.data.local.FiltersLocalDataSourceImpl
 import com.alancamargo.hearthstone.filters.data.remote.FiltersRemoteDataSource
 import com.alancamargo.hearthstone.filters.data.remote.FiltersRemoteDataSourceImpl
+import com.alancamargo.hearthstone.filters.data.repository.FiltersRepositoryImpl
+import com.alancamargo.hearthstone.filters.domain.repository.FiltersRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -25,4 +27,8 @@ internal abstract class FiltersModule {
     abstract fun bindFiltersLocalDataSource(
         impl: FiltersLocalDataSourceImpl
     ): FiltersLocalDataSource
+
+    @Binds
+    @ViewModelScoped
+    abstract fun bindFiltersRepository(impl: FiltersRepositoryImpl): FiltersRepository
 }
