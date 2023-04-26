e2d7e9631693f339abd85a7ac11a745faefc9b22
Alan Camargo <alan.uk.br@gmail.com>
2023-04-26 17:28:00 -0300

    Add api to dependency injection

--- feature-filters/src/main/java/com/alancamargo/hearthstone/filters/di/FiltersDataModule.kt
+++ feature-filters/src/main/java/com/alancamargo/hearthstone/filters/di/FiltersDataModule.kt
@@ -1,12 +1,20 @@
 package com.alancamargo.hearthstone.filters.di
 
+import com.alancamargo.hearthstone.core.network.ApiProvider
+import com.alancamargo.hearthstone.filters.data.api.FiltersApi
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 internal object FiltersDataModule {
 
-
+    @Provides
+    @Singleton
+    fun provideFiltersApi(
+        apiProvider: ApiProvider
+    ): FiltersApi = apiProvider.provideService(FiltersApi::class.java)
 }
