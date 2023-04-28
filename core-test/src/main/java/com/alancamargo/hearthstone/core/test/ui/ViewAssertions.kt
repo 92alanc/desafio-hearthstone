311f2e3fc11d7b938abbb465f06bc2c39bfa5809
Alan Camargo <alan.uk.br@gmail.com>
2023-04-28 12:06:36 -0300

    Create structure for UI tests

---
+++ core-test/src/main/java/com/alancamargo/hearthstone/core/test/ui/ViewAssertions.kt
@@ -0,0 +1,10 @@
+package com.alancamargo.hearthstone.core.test.ui
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withText
+
+fun assertTextIsDisplayed(text: String) {
+    onView(withText(text)).check(matches(isDisplayed()))
+}
