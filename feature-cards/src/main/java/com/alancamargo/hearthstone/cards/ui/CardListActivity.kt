b54dda08b4927eceab35e9020898864c0c3e397d
Alan Camargo <alan.uk.br@gmail.com>
2023-04-27 21:04:45 -0300

    Implement activity layout

---
+++ feature-cards/src/main/java/com/alancamargo/hearthstone/cards/ui/CardListActivity.kt
@@ -0,0 +1,10 @@
+package com.alancamargo.hearthstone.cards.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+internal class CardListActivity : AppCompatActivity() {
+
+
+}
